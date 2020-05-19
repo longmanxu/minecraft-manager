16238a0c21eb105a6837fe267d772c82499b6a76
Longman Xu <longmanxu@live.ca>
2020-05-18 20:54:36 -0400

    update stop time to 23:00

--- server/scripts/time-warning.sh
+++ server/scripts/time-warning.sh
@@ -2,7 +2,7 @@
 if screen -list | grep -q mc
 then
 	NOW=$(TZ="America/Toronto" date +%s) 
-	END=$(TZ="America/Toronto" date -d 'today 22:30:00' +%s)
+	END=$(TZ="America/Toronto" date -d 'today 23:00:00' +%s)
 	DIFF=$(($END-$NOW))
 	screen -p 0 -S mc -X eval "stuff \"say Shutting down in $((DIFF / 60)) min...\"\\015"
 fi
